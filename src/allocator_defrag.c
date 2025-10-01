5b98860b0772eadfaa64b3fa9d86d4c4e41dc6db
Alina Liu <liusalisa6363@gmail.com>
2025-10-01 18:37:37 +0000

    Document 1/8 rule for defrag decision
    Signed-off-by: Alina Liu <liusalisa6363@gmail.com>

--- src/allocator_defrag.c
+++ src/allocator_defrag.c
@@ -349,7 +349,8 @@ unsigned long allocatorDefragGetFragSmallbins(void) {
  *    defragmentation is not necessary as moving regions is guaranteed not to change the fragmentation ratio.
  * 2. If the number of non-full slabs (bin_usage->curr_nonfull_slabs) is less than 2, defragmentation is not performed
  *    because there is no other slab to move regions to.
- * 3. Always defrag if the slab is less than 1/8 full: ensure no small-size items left without defragged, fixed defrag didn't stop issue.
+ * 3. Defrag if the slab is less than 1/8 full to ensure no low-utilization slabs left without defragged, and stabilize the defrag behavior.
+ *    1/8 threshold (12.5%) chosen to align with existing utilization threshold factor.
  * 4. If slab utilization < 'avg utilization'*1.125 [code 1.125 == (1000+UTILIZATION_THRESHOLD_FACTOR_MILLI)/1000]
  *    than we should defrag. This is aligned with previous je_defrag_hint implementation.
  */
